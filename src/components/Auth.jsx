065d2352f4b4650025be24143657a50609a72d34
para-commando <anirudh.nayak.172k@gmail.com>
2024-05-21 23:27:44 +0530

    verifyOTP logic integ

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,46 +1,86 @@
-import React, { useState,useRef } from 'react'
-import firebase from '../firebase'
-
+import React, { useState, useRef } from 'react';
+import firebase from '../firebase';
 
 function Auth() {
-    const [mobileNumber, setMobileNumber] = useState('');
-    const [verificationNumber, setverificationNumber] = useState('')
-    const recaptchaRef = useRef(null);
-    const handleSendOtp = () => {
-        const firebaseConfig = {
-            apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-            authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-            projectId: import.meta.env.VITE_PROJECT_ID,
-            storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-            messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-            appId: import.meta.env.VITE_MESSAGING_APP_ID,
-            measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-          };
-           
-        if(recaptchaRef.current){
-            recaptchaRef.current.innerHTML = '<div id="recaptcha-container"> </div>'   
-            const verifier = new firebase.auth.RecaptchaVerifier('recaptcha-container',{size:'invisible'});
-            firebase.auth().signInWithPhoneNumber(mobileNumber,verifier).then((confirmationResult)=>{
-            setverificationNumber(confirmationResult.verificationId);
-            alert('OTP sent successfully')
-            }
-            ).catch((err) => {
-                console.log("🚀 ~ handleSendOtp ~ err:", err)
-        
-            })         
-        }
-      
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [verificationNumber, setverificationNumber] = useState('');
+  const [verificationCode, setverificationCode] = useState('');
+
+  const recaptchaRef = useRef(null);
+  const handleSendOtp = () => {
+    if (recaptchaRef.current) {
+      recaptchaRef.current.innerHTML = '<div id="recaptcha-container"> </div>';
+      const verifier = new firebase.auth.RecaptchaVerifier(
+        'recaptcha-container',
+        { size: 'invisible' }
+      );
+      firebase
+        .auth()
+        .signInWithPhoneNumber(mobileNumber, verifier)
+        .then((confirmationResult) => {
+          setverificationNumber(confirmationResult.verificationId);
+          alert('OTP sent successfully');
+        })
+        .catch((err) => {
+          console.log('🚀 ~ handleSendOtp ~ err:', err);
+        });
     }
+  };
+
+  const handleVerifyOtp = () => {
+    const creds = firebase.auth.PhoneAuthProvider.credential(
+      verificationNumber,
+      verificationCode
+    );
+    firebase
+      .auth()
+      .signInWithCredential(creds)
+      .then((userCredential) => {
+        console.log(
+          '🚀 ~ firebase.auth ~ JSON.stringify(userCredential)}:',
+          JSON.stringify(userCredential)
+        );
+        alert(
+          `Otp verified successfully for the user ${JSON.stringify(
+            userCredential
+          )}`
+        );
+      })
+      .catch((err) => {
+        console.log('🚀 ~ firebase.auth ~ err:', err);
+      });
+  };
   return (
     <div>
       <h1>mobile otp authentication</h1>
-      <div ref={recaptchaRef}>
+      <div ref={recaptchaRef}></div>
+      <div className='sendOtpSection'>
+        <input
+          type='tel'
+          placeholder='+919876543211'
+          value={mobileNumber}
+          onChange={(e) => {
+            setMobileNumber(e.target.value);
+          }}
+        />
+        <button onClick={handleSendOtp} style={{ backgroundColor: 'red' }}>
+          Send OTP
+        </button>
       </div>
-      <input type="tel" placeholder='+919876543211' value={mobileNumber} onChange={e=> { setMobileNumber(e.target.value)
-      }}/>
-       <button onClick={handleSendOtp} style={{backgroundColor:'red'}}>Send OTP</button>
+
+      <div className='verifyOtpSection'>
+        <input
+          type='text'
+          placeholder='Enter OTP'
+          value={verificationCode}
+          onChange={(e) => setverificationCode(e.target.value)}
+        />
+        <button onClick={handleVerifyOtp} style={{ backgroundColor: 'red' }}>
+          Verify OTP
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
